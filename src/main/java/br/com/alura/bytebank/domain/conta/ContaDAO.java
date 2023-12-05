89702a475b7d57d9f4cd9a45e1fe955b8ec1fa41
Vinicius <vini1068@gmail.com>
2023-12-04 21:37:13 -0300

    Implementado deletar e column esta_ativa

--- src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
+++ src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
@@ -1,6 +1,5 @@
 package br.com.alura.bytebank.domain.conta;
 
-import br.com.alura.bytebank.domain.RegraDeNegocioException;
 import br.com.alura.bytebank.domain.cliente.Cliente;
 import br.com.alura.bytebank.domain.cliente.DadosCadastroCliente;
 
@@ -22,7 +21,7 @@ public class ContaDAO {
 
     public void salvar(DadosAberturaConta dadosDaConta){
         var cliente = new Cliente(dadosDaConta.dadosCliente());
-        var conta = new Conta(dadosDaConta.numero(), BigDecimal.ZERO, cliente);
+        var conta = new Conta(dadosDaConta.numero(), BigDecimal.ZERO, cliente,true);
 
         String sql = "INSERT INTO conta (numero, saldo, cliente_nome, cliente_cpf, cliente_email)" +
                 "VALUES (?, ?, ?, ?, ?)";
@@ -34,6 +33,7 @@ public class ContaDAO {
             preparedStatement.setString(3, dadosDaConta.dadosCliente().nome());
             preparedStatement.setString(4, dadosDaConta.dadosCliente().cpf());
             preparedStatement.setString(5, dadosDaConta.dadosCliente().email());
+            preparedStatement.setBoolean(6,true);
 
             preparedStatement.execute();
             preparedStatement.close();
@@ -48,7 +48,7 @@ public class ContaDAO {
         ResultSet resultSet;
         Set<Conta> contas = new HashSet<>();
 
-        String sql = "SELECT * FROM conta";
+        String sql = "SELECT * FROM conta WHERE esta_ativa = true";
 
         try {
             ps = conn.prepareStatement(sql);
@@ -60,11 +60,12 @@ public class ContaDAO {
                 String nome = resultSet.getString(3);
                 String cpf = resultSet.getString(4);
                 String email = resultSet.getString(5);
+                Boolean estaAtiva = resultSet.getBoolean(6);
 
                 DadosCadastroCliente dadosCadastroCliente = new DadosCadastroCliente(nome, cpf, email);
                 Cliente cliente = new Cliente(dadosCadastroCliente);
 
-                contas.add(new Conta(numero, saldo, cliente));
+                contas.add(new Conta(numero, saldo, cliente, estaAtiva));
             }
             resultSet.close();
             ps.close();
@@ -79,7 +80,7 @@ public class ContaDAO {
         PreparedStatement ps;
         ResultSet resultSet;
 
-        String sql = "SELECT * FROM conta WHERE numero = ?";
+        String sql = "SELECT * FROM conta WHERE numero = ? and esta_ativa = true";
         Conta conta = null;
         try {
             ps = conn.prepareStatement(sql);
@@ -92,11 +93,12 @@ public class ContaDAO {
                 String nome = resultSet.getString(3);
                 String cpf = resultSet.getString(4);
                 String email = resultSet.getString(5);
+                Boolean estaAtiva = resultSet.getBoolean(6);
 
                 DadosCadastroCliente dadosCadastroCliente = new DadosCadastroCliente(nome, cpf, email);
                 Cliente cliente = new Cliente(dadosCadastroCliente);
 
-                conta = new Conta(numero, saldo, cliente);
+                conta = new Conta(numeroRecuperado, saldo, cliente, estaAtiva);
             }
             resultSet.close();
             ps.close();
@@ -117,6 +119,46 @@ public class ContaDAO {
 
             ps.setBigDecimal(1, valor);
             ps.setInt(2, numero);
+
+            ps.execute();
+            conn.commit();
+            ps.close();
+            conn.close();
+        }catch (SQLException e){
+            try{
+                conn.rollback();
+            } catch (SQLException ex){
+                throw new RuntimeException(ex);
+            }
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deletar(Integer numeroDaConta){
+        String sql = "DELETE FROM conta WHERE numero = ?";
+
+        try{
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setInt(1, numeroDaConta);
+
+            ps.execute();
+            ps.close();
+            conn.close();
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void alterarLogico(Integer numeroDaConta){
+        PreparedStatement ps;
+        String sql = "UPDATE conta SET esta_ativa = false WHERE numero = ?";
+
+        try{
+            conn.setAutoCommit(false);
+            ps = conn.prepareStatement(sql);
+
+             ps.setInt(1, numeroDaConta);
 
             ps.execute();
             conn.commit();
--- src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
+++ src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
@@ -2,13 +2,9 @@ package br.com.alura.bytebank.domain.conta;
 
 import br.com.alura.bytebank.ConnectionFactory;
 import br.com.alura.bytebank.domain.RegraDeNegocioException;
-import br.com.alura.bytebank.domain.cliente.Cliente;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.HashSet;
 import java.util.Set;
 
 public class ContaService {
@@ -18,8 +14,6 @@ public class ContaService {
     public ContaService(){
         this.connection = new ConnectionFactory();
     }
-
-    private Set<Conta> contas = new HashSet<>();
 
     public Set<Conta> listarContasAbertas() {
         Connection conn = connection.recuperarConexao();
@@ -50,6 +44,11 @@ public class ContaService {
         if (valor.compareTo(conta.getSaldo()) > 0) {
             throw new RegraDeNegocioException("Saldo insuficiente!");
         }
+
+        if(!conta.getEstaAtiva()){
+            throw new RegraDeNegocioException("Conta não está ativa");
+        }
+
         BigDecimal novoValor = conta.getSaldo().subtract(valor);
         alterar(conta, novoValor);
     }
@@ -58,6 +57,10 @@ public class ContaService {
         var conta = buscarContaPorNumero(numeroDaConta);
         if (valor.compareTo(BigDecimal.ZERO) <= 0) {
             throw new RegraDeNegocioException("Valor do deposito deve ser superior a zero!");
+        }
+
+        if(!conta.getEstaAtiva()){
+            throw new RegraDeNegocioException("Conta não está ativa");
         }
 
         BigDecimal novoValor = conta.getSaldo().add(valor);
@@ -75,7 +78,20 @@ public class ContaService {
             throw new RegraDeNegocioException("Conta não pode ser encerrada pois ainda possui saldo!");
         }
 
-        contas.remove(conta);
+        Connection conn = connection.recuperarConexao();
+
+        new ContaDAO(conn).deletar(numeroDaConta);
+    }
+
+    public void encerrarLogico(Integer numeroDaConta){
+        var conta = buscarContaPorNumero(numeroDaConta);
+        if (conta.possuiSaldo()) {
+            throw new RegraDeNegocioException("Conta não pode ser encerrada pois ainda possui saldo!");
+        }
+
+        Connection conn = connection.recuperarConexao();
+
+        new ContaDAO(conn).alterarLogico(numeroDaConta);
     }
 
     private Conta buscarContaPorNumero(Integer numero) {
